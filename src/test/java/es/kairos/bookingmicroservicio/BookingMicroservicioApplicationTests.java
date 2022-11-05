df84e0a590f1e22dce8db5453614f05c2c720119
DaniMek <danibermudezrodriguez@gmail.com>
2022-11-05 19:06:58 +0100

    initial commit

---
+++ src/test/java/es/kairos/bookingmicroservicio/BookingMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package es.kairos.bookingmicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
